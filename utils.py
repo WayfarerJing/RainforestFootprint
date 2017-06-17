d049074757ffcbd1b49d64c0af9a67f9f80b8360
wayfarerjing <jingxjy@hotmail.com>
2017-06-16 23:00:29 -0400

    update load method for suitable dimension

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 def load_rgb(infile):
   all_file_paths = [f for f in listdir(infile) if isfile(join(infile,f))]
-  images = np.empty(len(all_file_paths) - 1, dtype=object)
+  images = np.empty((len(all_file_paths) - 1, 256, 256, 3))
   for n in range(1, len(all_file_paths)):
-    images[n - 1] = cv2.imread(join(infile,all_file_paths[n]))
+    images[n - 1,:,:,:] = cv2.imread(join(infile,all_file_paths[n]))
   return images

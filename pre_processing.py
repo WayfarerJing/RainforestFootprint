8cfab7a839f30fc6165c11bce85f0246cca55112
wayfarerjing <jingxjy@hotmail.com>
2017-06-17 09:55:04 -0400

    Create pre_processing.py

---
+++ pre_processing.py
@@ -0,0 +1,23 @@
+import numpy as np
+import cv2
+from utils import *
+
+def my_PreProc(data):
+    assert(len(data.shape)==4)
+    assert (data.shape[1]==3)  #Use the original images
+    #black-white conversion
+    train_imgs = rgb2gray(data)
+    #my preprocessing:
+    train_imgs = dataset_normalized(train_imgs)
+
+# ===== normalize over the dataset
+def dataset_normalized(imgs):
+    assert (len(imgs.shape)==4)  #4D arrays
+    assert (imgs.shape[3]==1)  #check the channel is 1
+    imgs_normalized = np.empty(imgs.shape)
+    imgs_std = np.std(imgs)
+    imgs_mean = np.mean(imgs)
+    imgs_normalized = (imgs-imgs_mean)/imgs_std
+    for i in range(imgs.shape[0]):
+        imgs_normalized[i] = ((imgs_normalized[i] - np.min(imgs_normalized[i])) / (np.max(imgs_normalized[i])-np.min(imgs_normalized[i])))*255
+    return imgs_normalized

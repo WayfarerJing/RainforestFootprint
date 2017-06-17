bc1fff24660f6788f160feb1c421547bae180306
wayfarerjing <jingxjy@hotmail.com>
2017-06-17 17:36:53 -0400

    Update utils.py

--- libs/utils.py
+++ libs/utils.py
@@ -13,10 +13,3 @@ def load_rgb(infile):
     images[n - 1,:,:,:] = cv2.imread(join(infile,all_file_paths[n]))
   return images
 
-#convert RGB image in black and white (# of samples, 256,256,1)
-def rgb2gray(rgb):
-    assert (len(rgb.shape)==4)  #4D arrays
-    assert (rgb.shape[3]==3)
-    bn_imgs = rgb[:,:,:,0]*0.299 + rgb[:,:,:,1]*0.587 + rgb[:,:,:,2]*0.114
-    bn_imgs = np.reshape(bn_imgs,(rgb.shape[0],rgb.shape[1],rgb.shape[2],1))
-    return bn_imgs

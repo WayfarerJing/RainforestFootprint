851f956dc45710d7148690e01abda704ea9bc2f5
wayfarerjing <jingxjy@hotmail.com>
2017-06-19 22:14:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ config.read('config_file_path')
 path_dir = config.get('data paths', 'path_dir')
 train_sample = config.get('data paths', 'train_sample')
 train_tag = config.get('data paths', 'train_tag')
-#Experiment name
-name_experiment = config.get('experiment name', 'name')
+
 #training settings
 N_epochs = int(config.get('training settings', 'N_epochs'))
 batch_size = int(config.get('training settings', 'batch_size'))
@@ -23,12 +22,16 @@ patches_train_img_sample, patches_train_tag_sample = get_data_training_sample(
 n_ch = patches_train_img_sample.shape[3]
 patch_height = patches_train_img_sample.shape[1]
 patch_width = patches_train_img_sample.shape[2]
+
 model = get_unet(n_ch, patch_height, patch_width)  #the U-net model
+
 print "Check: final output of the network:"
 print model.output_shape
+
 #TODO: add check pointer
 
 model.fit(patches_train_img_sample, patches_train_tag_sample, batch_size=32, epochs=N_epochs, verbose=1, shuffle=True,
               validation_split=0.2)
 score = model.evaluate(patches_train_img_sample, patches_train_tag_sample, batch_size=16)
+
 #TODO: save model

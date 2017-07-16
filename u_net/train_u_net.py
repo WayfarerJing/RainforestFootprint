94e436c5e8eb78235ca93bd12a1899e551939b25
wayfarerjing <jingxjy@hotmail.com>
2017-07-15 22:08:04 -0400

    Update train_u_net.py

--- u_net/train_u_net.py
+++ u_net/train_u_net.py
@@ -55,3 +55,86 @@ def get_unet(n_ch, patch_height, patch_width):
     model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy', f1_score])
     
     return model
+
+def get_unet_2(n_ch, patch_height, patch_width):
+    inputs = Input((patch_height, patch_width, n_ch))
+    inputs = Convolution2D(32, 3, 3, activation='relu', padding = (1,1), border_mode='same')(inputs)
+    
+    conv1 = BatchNormalization(inputshape = ((patch_height, patch_height, n_ch)))(inputs)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv1)
+    conv1 = BatchNormalization()(pool1)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv1)
+    conv1 = BatchNormalization()(pool1)
+    conv1 = Convolution2D(32, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = BatchNormalization()(pool1)
+    conv2 = Convolution2D(64, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Convolution2D(64, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv2)
+    conv2 = BatchNormalization()(conv2)
+    conv2 = Convolution2D(64, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = BatchNormalization()(pool2)
+    conv3 = Convolution2D(128, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Convolution2D(128, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv3)
+    conv3 = BatchNormalization()(conv3)
+    conv3 = Convolution2D(128, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = BatchNormalization()(pool3)
+    conv4 = Convolution2D(256, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv4)
+    conv4 = BatchNormalization()(pool1)
+    conv4 = Convolution2D(256, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv4)
+    conv4 = BatchNormalization()(pool1)
+    conv4 = Convolution2D(256, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = BatchNormalization()(pool4)
+    conv5 = Convolution2D(512, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Convolution2D(512, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv5)
+    conv5 = BatchNormalization()(conv5)
+    conv5 = Conv2DTranspose(512, 3, 3, activation='relu', stride = (2,2), padding = (1,1), border_mode='same')(conv5)
+    
+    up6 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=3)
+    conv6 = BatchNormalization()(up6)
+    conv6 = Convolution2D(256, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Convolution2D(256, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv6)
+    conv6 = BatchNormalization()(conv6)
+    conv6 = Conv2DTranspose(256, 3, 3, activation='relu', stride = (2,2), padding = (1,1), border_mode='same')(conv6)
+    
+    up7 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=3)
+    conv7 = BatchNormalization()(up7)
+    conv7 = Convolution2D(128, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Convolution2D(128, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv7)
+    conv7 = BatchNormalization()(conv7)
+    conv7 = Conv2DTranspose(128, 3, 3, activation='relu', stride = (2,2), padding = (1,1), border_mode='same')(conv7)
+    
+    up8 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=3)
+    conv8 = BatchNormalization()(up8)
+    conv8 = Convolution2D(64, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Convolution2D(64, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv8)
+    conv8 = BatchNormalization()(conv8)
+    conv8 = Conv2DTranspose(64, 3, 3, activation='relu', stride = (2,2), padding = (1,1), border_mode='same')(conv8)
+    
+    up9 = merge([UpSampling2D(size=(2, 2))(conv8), conv1], mode='concat', concat_axis=3)
+    conv9 = BatchNormalization()(up9)
+    conv9 = Convolution2D(32, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv9)
+    conv9 = BatchNormalization()(conv9)
+    conv9 = Convolution2D(32, 3, 3, activation='relu', padding = (1,1), border_mode='same')(conv9)
+  
+    conv10 = Convolution2D(1, 1, 1, activation='relu', padding = (0,0), border_mode='same')(conv9)
+
+    predictions = Dense(2, activation='sigmoid')(conv10)
+    model = Model(input=inputs, output=predictions)
+    
+    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='binary_crossentropy', metrics=['accuracy', f1_score])
+    
+    return model
